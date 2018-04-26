3353f74a7b9f79d00ff6abeb351f8cb88dfc9c5a
bramulous <>
2018-04-26 08:48:28 +0200

    Created module

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    map: {
+        '*': {
+            ajaxQty: 'Module_AjaxCart/js/cartAjaxQty'
+        }
+    }
+};
---
+++ view/frontend/web/js/cartAjaxQty.js
@@ -0,0 +1,58 @@
+define([
+    'jquery',
+    'Magento_Checkout/js/action/get-totals',
+    'Magento_Customer/js/customer-data'
+], function ($, getTotalsAction, customerData) {
+
+    $(document).ready(function(){
+        console.log("running");
+
+
+        // On change function for updating totals
+        $(document).on('change', 'input[name$="[qty]"]', function(){
+                console.log("quantity changed");
+                var form = $($(this).closest('form'));
+
+                $.ajax({
+                    url: form.attr('action'),
+                    data: form.serialize(),
+                    showLoader: true,
+                    success: function (res) {
+                        var parsedResponse = $.parseHTML(res);
+                        var result = $(parsedResponse).find("#form-validate");
+                        var sections = ['cart'];
+
+                        // Replacing new subtotals per item
+                        $("#form-validate").replaceWith(result);
+
+                        // Reloading the mini cart
+                        customerData.reload(sections, true);
+
+                        // Reloading the totals summary block
+                        var deferred = $.Deferred();
+                        getTotalsAction([], deferred);
+
+                        console.log('Ajax succes function completed');
+
+                    },
+                    error: function (xhr, status, error) {
+                        var err = eval("(" + xhr.responseText + ")");
+                        console.log(err.Message);
+                    }
+                });
+
+            }
+        );
+
+        $('.cart.table-wrapper .action-delete').on("click", function (e) {
+            e.preventDefault();
+            var cartItem = $(this);
+            $('input[name$="[qty]"]').val('0');
+            alert($(cartItem).data().bind);
+            return false;
+
+        });
+
+
+    });
+});
