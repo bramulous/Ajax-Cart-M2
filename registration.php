249c1a9654c8faafb8764bf4c604fff03cfec6e6
bramulous <>
2018-05-29 14:06:24 +0200

    minor change

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Module_AjaxCart',
+    'BRVM_AjaxCart',
     __DIR__
 );
